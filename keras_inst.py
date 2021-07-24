fc78a0b900c479c91e0a3e79bf114d0d0179deba
snsk <shinsk@gmail.com>
2021-07-25 08:42:59 +0900

    update

---
+++ keras_inst.py
@@ -0,0 +1,9 @@
+# TensorFlow と tf.keras のインポート
+import tensorflow as tf
+from tensorflow import keras
+
+# ヘルパーライブラリのインポート
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
