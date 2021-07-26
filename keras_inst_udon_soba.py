56be81698308c1b331694ec5fcff37e9d8303b87
snsk <shinsk@gmail.com>
2021-07-26 11:45:52 +0900

    update

---
+++ keras_inst_udon_soba.py
@@ -0,0 +1,95 @@
+import keras
+from keras.utils import np_utils, plot_model
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+import numpy as np
+from sklearn.model_selection import train_test_split
+from PIL import Image
+import glob
+
+folder = ["ramen","hiyashi"]
+image_size = 50
+
+X = []
+Y = []
+for index, name in enumerate(folder):
+    dir = "./" + name
+    files = glob.glob(dir + "/*.jpg")
+    for i, file in enumerate(files):
+        image = Image.open(file)
+        image = image.convert("RGB")
+        image = image.resize((image_size, image_size))
+        data = np.asarray(image)
+        X.append(data)
+        Y.append(index)
+
+X = np.array(X)
+Y = np.array(Y)
+
+X = X.astype('float32')
+X = X / 255.0
+
+# 正解ラベルの形式を変換
+Y = np_utils.to_categorical(Y, 2)
+
+# 学習用データとテストデータ
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.20)
+'''
+X_train:訓練用入力データ
+X_test :訓練用正解データ
+Y_train:評価用入力データ
+Y_test :評価用正解データ
+'''
+
+# CNNを構築
+# Sequentialモデルは addされたモデル層が一列に並ぶ
+model = Sequential()
+
+#Conv2D:2次元畳み込み層 3x3のパネルを32枚使う。padding='same' で出力画像のサイズが変わらないようにする
+model.add(Conv2D(32, (3, 3), padding='same',input_shape=X_train.shape[1:], activation='relu'))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(2, activation='softmax'))
+
+#モデルのコンパイル
+model.compile(
+    loss='categorical_crossentropy',
+    optimizer='sgd',
+    metrics=['accuracy']
+)
+
+#モデルの訓練
+epochs_num=200
+result = model.fit(X_train, Y_train, epochs=epochs_num, validation_data=(X_test, Y_test))
+
+#modelのビジュアル可視化。要install and PATH setting GraphViz, pydot
+#functional API を利用した複雑なモデルの時はこちら
+plot_model(model, to_file='./model.png', show_shapes=True, expand_nested=True)
+#modelのテキスト可視化。Sequentialモデルならこれでも十分、らしい
+#print(model.summary())
+
+import matplotlib.pyplot as plt
+ 
+plt.plot(range(1, epochs_num+1), result.history['acc'], label="training")
+plt.plot(range(1, epochs_num+1), result.history['val_acc'], label="validation")
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+#評価 & 評価結果出力
+#print(model.evaluate(X_test, Y_test))
+#print('Test:')
+#test_loss, test_acc = model.evaluate(X_test, Y_test, verbose=1)
+#print('\nTest accuracy:', test_acc)
